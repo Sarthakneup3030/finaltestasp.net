afc150dfa57f2323327644f8f94b7b11f4516c89
Sarthakneup3030 <137415871+Sarthakneup3030@users.noreply.github.com>
2023-08-14 14:36:29 -0400

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using finaltest200535820.Models;  // Ensure to include the namespace where Student model is defined
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    // DbSet for car
+    public DbSet<Car> Cars { get; set; }
+
+}
