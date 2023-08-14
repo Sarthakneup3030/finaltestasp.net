425e912b916efd4941e35beecae8faa6abad3f63
Sarthakneup3030 <137415871+Sarthakneup3030@users.noreply.github.com>
2023-08-14 14:36:10 -0400

    Add files via upload

---
+++ car.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Car
+{
+    [Required]
+    public string ChasisNo { get; set; }
+
+    [Required]
+    public string CarName { get; set; }
+
+    [Required]
+    public string CompanyName { get; set; }
+
+    [Required]
+    public string CarType { get; set; }
+
+    [Display(Name = "Email Address")]
+    public string EmailAddress { get; set; }
+}
